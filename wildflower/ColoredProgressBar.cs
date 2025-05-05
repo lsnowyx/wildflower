4f49754beac787dc0ee62f8c991353b7dbe5d26b
lsnowyx <ftjfdukn@gmail.com>
2025-05-05 21:58:57 +0300

    v0.5 fixed custom p_bar shimmering black

--- wildflower/ColoredProgressBar.cs
+++ wildflower/ColoredProgressBar.cs
@@ -5,18 +5,23 @@
         public ColoredProgressBar()
         {
             SetStyle(ControlStyles.UserPaint, true);
+            DoubleBuffered = true;
         }
         protected override void OnPaint(PaintEventArgs e)
         {
             Rectangle rect = ClientRectangle;
             Graphics g = e.Graphics;
-            rect.Width = (int)((float)Value / Maximum * Width);
-            if (rect.Width > 0)
+            using (SolidBrush bg = new SolidBrush(Color.Black))
+            {
+                g.FillRectangle(bg, rect);
+            }
+            int fillWidth = (int)((float)this.Value / this.Maximum * this.Width);
+            if (fillWidth > 0)
             {
                 Color myColor = ColorTranslator.FromHtml("#00FFFF");
                 using (SolidBrush brush = new SolidBrush(myColor))
                 {
-                    g.FillRectangle(brush, 0, 0, rect.Width, Height);
+                    g.FillRectangle(brush, 0, 0, fillWidth, Height);
                 }
             }
         }
