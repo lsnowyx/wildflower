a76ee141a9b4909c86fee2475c83754606e1e427
lsnowyx <ftjfdukn@gmail.com>
2025-05-25 23:09:11 +0300

    made opening multiple instances of the application impossible

--- wildflower/Program.cs
+++ wildflower/Program.cs
@@ -2,6 +2,7 @@ namespace wildflower
 {
     internal static class Program
     {
+        private static Mutex mutex;
         /// <summary>
         ///  The main entry point for the application.
         /// </summary>
@@ -10,15 +11,16 @@ namespace wildflower
         {
             // To customize application configuration such as set high DPI settings or default font,
             // see https://aka.ms/applicationconfiguration.
+
+            const string appName = "wildflower";
+            bool createdNew;
+            mutex = new Mutex(true, appName, out createdNew);
+            if (!createdNew) return;
+
             ApplicationConfiguration.Initialize();
-            try
-            {
-                Application.Run(new Form1());
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show($"If you see this please screenshot this error message and report it on github asap, thx :D\n{ex.Message}");
-            }
+            Application.Run(new Form1());
+
+            mutex.ReleaseMutex();
         }
     }
 }
